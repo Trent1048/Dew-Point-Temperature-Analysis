deba1bcd86cc30703eb3a6d35cf76fcbd689d654
Trent1048 <trentbultsma@gmail.com>
2023-11-08 20:26:12 -0800

    convert rdd implementation to use float/int math instead of strings

--- spark rdd/dew_temp.py
+++ spark rdd/dew_temp.py
@@ -8,24 +8,23 @@ temperatureInput = sc.textFile("./normal_hly_sample_temperature.csv")
 def mapTemperatureLine(line: str):
     tokens = line.split(",")
     if tokens[0] == "STATION":
-        return ("NULL", "0, 0, 0")
+        return ("NULL", (0, 0, 0))
     
     date = tokens[5].split(" ")[0]
-    normalTemp = tokens[6]
-    dewTemp = tokens[7]
-    temperatures = normalTemp + ", " + dewTemp
+    normalTemp = float(tokens[6])
+    dewTemp = float(tokens[7])
 
-    return (date, temperatures + ", 1")
+    return (date, (normalTemp, dewTemp, 1))
 
 def reduceTemperatureAverage(tempVal1, tempVal2):
-    normalTemp1, dewTemp1, count1 = tempVal1.split(", ")
-    normalTemp2, dewTemp2, count2 = tempVal2.split(", ")
+    normalTemp1, dewTemp1, count1 = tempVal1
+    normalTemp2, dewTemp2, count2 = tempVal2
 
-    resultNormalTemp = str(float(normalTemp1) + float(normalTemp2))
-    resultDewTemp = str(float(dewTemp1) + float(dewTemp2))
-    resultCount = str(int(count1) + int(count2))
+    resultNormalTemp = normalTemp1 + normalTemp2
+    resultDewTemp = dewTemp1 + dewTemp2
+    resultCount = count1 + count2
 
-    return resultNormalTemp + ", " + resultDewTemp + ", " + resultCount
+    return (resultNormalTemp, resultDewTemp, resultCount)
 
 #def collectTemperatureAverage(tempVal1, tempVal2):
 #    avgNormalTemp1, avgDewTemp1, count1 = tempVal1.split(", ")
